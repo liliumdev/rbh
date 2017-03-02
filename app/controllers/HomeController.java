9fbbcfbb571ddf45b4eac3331f040fde3a1c81ac
Kerim Popovic <ahmed.popovic@gmail.com>
2017-03-02 12:13:36 +0100

    added catch-all route to redirect to index

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,7 +4,10 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 public class HomeController extends Controller {
-   public Result index() {
+    public Result index() {
+        return ok(views.html.main.render());
+    }
+    public Result catchAll(String path) {
        return ok(views.html.main.render());
    }
 }
