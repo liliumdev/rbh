5d41500a00a14bc4a1cfa4b6c4e0a5a2bbcc15e1
Ahmed <ahmed.popovic@gmail.com>
2017-03-13 11:37:47 +0100

    backend improvements regarding controllers and packages

--- app/controllers/CityController.java
+++ app/controllers/CityController.java
@@ -10,50 +10,6 @@ import services.exceptions.ServiceException;
 import static play.libs.Json.toJson;
 
 public class CityController extends BaseController<City, CityService> {
-
-    @Transactional
-    @SecureAuth.Authenticated(roles={"ADMIN"})
-    public Result create(Long countryId)
-    {
-        try {
-            Form<City> form = formFactory.form(City.class).bindFromRequest();
-            if(form.hasErrors())
-                return badRequest(form.errorsAsJson());
-
-            return created(toJson(service.create(countryId, form.get())));
-        } catch(ServiceException e) {
-            return badRequest("Service error in CityController@create");
-        } catch (Exception e) {
-            return internalServerError("Internal server error in CityController@create");
-        }
-    }
-
-    @Transactional
-    @SecureAuth.Authenticated(roles={"ADMIN"})
-    public Result update(Long id) {
-        return super.update(id);
-    }
-
-    @Transactional
-    @SecureAuth.Authenticated(roles={"ADMIN"})
-    public Result delete(Long id)
-    {
-        try {
-            // Are there any restaurants in this city?
-            City city = service.get(id);
-            Integer numOfRestaurants = city.getRestaurants().size();
-            if(numOfRestaurants > 0)
-                return badRequest("Can't delete this city. There are " + numOfRestaurants +
-                                    " existing restaurants in this city!");
-
-            return super.delete(id);
-        } catch(ServiceException e) {
-            return badRequest("Service error in CityController@delete");
-        } catch (Exception e) {
-            return internalServerError("Internal server error in CityController@delete");
-        }
-    }
-
     @Transactional
     public Result allFromCountry(Long countryId)
     {
---
+++ app/controllers/admin/AccountController.java
@@ -0,0 +1,65 @@
+package controllers.admin;
+
+import controllers.BaseAdminController;
+import controllers.SecureAuth;
+import controllers.forms.admin.AccountCreationForm;
+import models.Account;
+import play.data.Form;
+import play.db.jpa.Transactional;
+import play.libs.Json;
+import play.mvc.BodyParser;
+import play.mvc.Result;
+import services.AccountService;
+import services.exceptions.ServiceException;
+
+public class AccountController extends BaseAdminController<Account, AccountService> {
+    @Transactional
+    @SecureAuth.Authenticated(roles={"ADMIN"})
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result create(Integer role) {
+        try {
+            // Validate the registration form
+            Form<AccountCreationForm> form = formFactory.form(AccountCreationForm.class).bindFromRequest();
+
+            // Is this email already present in the account table?
+            if(service.getByEmail(form.get().getEmail()) != null) {
+                form.errors().put("email", ValidationHelper.singleError("email", "This email already exists."));
+            }
+
+            if(form.hasErrors())
+                return badRequest(form.errorsAsJson());
+
+            // Currently only roles allowed are NORMAL and ADMIN, that is interval [0, 1]
+            if(!(role >= 0 && role <= 1))
+                return badRequest(Json.toJson("Unknown role!"));
+
+            // The account has been created
+            return created(Json.toJson(service.create(form.get(), role)));
+        } catch (ServiceException e) {
+            return internalServerError(Json.toJson("Internal server error in AccountController@create"));
+        }
+    }
+
+    @Transactional
+    @SecureAuth.Authenticated(roles={"ADMIN"})
+    public Result demote(Long id)
+    {
+        try {
+            return ok(Json.toJson(service.giveRole(id, AccountService.USER_TYPE.NORMAL.getValue())));
+        } catch (ServiceException e) {
+            return internalServerError(Json.toJson("Internal server error in AccountController@demote"));
+        }
+    }
+
+    @Transactional
+    @SecureAuth.Authenticated(roles={"ADMIN"})
+    public Result promote(Long id)
+    {
+        try {
+            return ok(Json.toJson(service.giveRole(id, AccountService.USER_TYPE.ADMIN.getValue())));
+        } catch (ServiceException e) {
+            return internalServerError(Json.toJson("Internal server error in AccountController@promote"));
+        }
+    }
+
+}
